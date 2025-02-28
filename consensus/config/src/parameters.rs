4d917d9d32df1a9c67fc33c8c543dc6fcabab372
NaitsabesMue <51112618+NaitsabesMue@users.noreply.github.com>
2025-02-28 08:20:53 +0100

    [Consensus] upstream changes: increase min round delay in tests #19644

--- consensus/config/src/parameters.rs
+++ consensus/config/src/parameters.rs
@@ -92,6 +92,9 @@ impl Parameters {
             // simtest is single threaded, and spending too much time in
             // consensus can lead to starvation elsewhere.
             Duration::from_millis(400)
+        } else if cfg!(test) {
+            // Avoid excessive CPU, data and logs in tests.
+            Duration::from_millis(250)
         } else {
             Duration::from_millis(50)
         }
