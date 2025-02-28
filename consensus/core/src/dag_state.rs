91651f648ea26e1656d3847af9caad44fb263f3e
ilyavorobev <vorobyev.i.v@yandex.ru>
2025-02-28 09:09:33 +0100

    change index display in new tests; it was done for old tests in previous commits

--- consensus/core/src/dag_state.rs
+++ consensus/core/src/dag_state.rs
@@ -1413,7 +1413,7 @@ mod test {
 
     #[tokio::test]
     #[should_panic(
-        expected = "Attempted to check for slot B3 that is <= the last gc evicted round 3"
+        expected = "Attempted to check for slot [1]3 that is <= the last gc evicted round 3"
     )]
     async fn test_contains_cached_block_at_slot_panics_when_ask_out_of_range_gc_enabled() {
         /// Keep 2 rounds from the highest committed round. This is considered
@@ -1985,7 +1985,7 @@ mod test {
 
     #[tokio::test]
     #[should_panic(
-        expected = "Attempted to request for blocks of rounds < 2, when the last evicted round is 1 for authority C"
+        expected = "Attempted to request for blocks of rounds < 2, when the last evicted round is 1 for authority [2]"
     )]
     async fn test_get_cached_last_block_per_authority_requesting_out_of_round_range_gc_enabled() {
         // GIVEN
